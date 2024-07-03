5fbc4a12d04bfa41f39f442279d5f0c72d0d474c
JanviChincholkar <janvichincholkar14@gmail.com>
2024-07-03 15:34:26 +0530

    code

---
+++ src/components/StudentCard/StudentCard.js
@@ -0,0 +1,9 @@
+import "./StudentCard.css"
+
+function StudentCard() {
+    return (
+        <div> <h1> StudentCard</h1></div>
+    )
+}
+
+export default StudentCard
---
+++ src/views/Home/Home.js
@@ -0,0 +1,13 @@
+import  "./Home.css"
+import StudentCard from "./../../components/StudentCard/StudentCard"
+const Home = () => {
+return (
+    <>
+    <h1>Student Cards</h1>
+
+    <StudentCard/>
+    </>
+)
+}
+
+export default Home
